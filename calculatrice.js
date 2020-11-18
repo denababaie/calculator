dc3b8010b782bf49abe1d65328f4024ebd8984c5
denababaie <72995019+denababaie@users.noreply.github.com>
2020-11-18 09:58:07 +0100

    changes here and there

--- calculatrice.js
+++ calculatrice.js
@@ -2,14 +2,16 @@
 
 const html = document.querySelector("html");
 const body = document.querySelector("body");
-const h1 = document.querySelector("h1");
+/*const h1 = document.querySelector("h1");
 const calculator = document.createElement("div");
 
 const display = document.querySelector("#display");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equalTo = document.querySelector(".equal-to");
-const clear = document.querySelector(".clear");
+const clear = document.querySelector(".clear");*/
+
+
 
 
 /* linken van html met js */
@@ -43,14 +45,14 @@ function operatorState(event) {
   firstNumText = display.value;
   operator = event.target.textContent;
   display.value = "";
-}
+} /* use display value to display what i want when clicking on a certain operator */
 
 function calculateState(event) {
     let firstNumInt = parseFloat(firstNumText);
     let secondNumInt = parseFloat(display.value);
     let result;
 
-/* not parseInt (integer) but parseFloat if I want to have numbers with comma*/
+/* changed parseInt (integer) to parseFloat! and got numbers with commas*/
 
     if (operator == "+") {
       result = firstNumInt + secondNumInt;
@@ -66,11 +68,19 @@ function calculateState(event) {
       result = firstNumInt * secondNumInt;
     }
 
-    if (operator == "^") {
+   if (operator == "^") {
       result = firstNumInt ** secondNumInt;
-    }
+    } 
+    /* "**" is the equivalent of "^" in js*/ 
 
-/* "**" is the equivalent of "^" in js*/ 
+    if (operator == "+/-") {
+      result = ((firstNumInt -  firstNumInt) - firstNumInt);
+    } 
+/* logic used: if I want a negative number ; I choose a random number (for example 4) then I take that number minus exact 
+same (amount) number ; so i end up getting (4-4); my result is now 0 ; so I end up doing the following (0-4); result = -4 */
+
+/* ( (number - number) - number ) */
+
 
 
     display.value = result;
